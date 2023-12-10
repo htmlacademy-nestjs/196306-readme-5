032b8df422f00a78d1417ab053f9c3c3573e38b9
Lena Rashkovan <lena.rashkovan@free-now.com>
2023-12-10 14:10:16 +0100

    feat(account): inject blog-user module as a dependency

--- project/apps/account/src/app/authentication/authentication.module.ts
+++ project/apps/account/src/app/authentication/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BlogUserModule } from '../blog-user/blog-user.module';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
+  imports: [BlogUserModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService]
 })
