b85c9e58b1f1efeb33b084f6e7afe21231a41710
Lena Rashkovan <lena.rashkovan@free-now.com>
2023-12-10 14:18:52 +0100

    feat(account): add registration

--- project/apps/account/src/app/authentication/authentication.service.ts
+++ project/apps/account/src/app/authentication/authentication.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { BlogUserRepository } from '../blog-user/blog-user.repository';
+import { BlogUserEntity } from '../blog-user/blog-user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AUTH_USER_EXISTS } from './authentication.constant';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly blogUserRepository: BlogUserRepository
   ) {}
+
+  public async register(dto: CreateUserDto) {
+    const {email, name, password} = dto;
+
+    const blogUser = {
+      email,
+      name,
+      avatar: '',
+      passwordHash: ''
+    };
+
+    const existingUser = await this.blogUserRepository.findByEmail(email);
+
+    if (existingUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser)
+      .setPassword(password)
+
+    return this.blogUserRepository
+      .save(userEntity);
+  }
 }
