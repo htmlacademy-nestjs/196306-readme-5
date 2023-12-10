f3ad4311d9726c973ccd11437a7e4d7d3c13a18c
Lena Rashkovan <lena.rashkovan@free-now.com>
2023-12-10 14:08:44 +0100

    feat(account): use blog-user repository as a provider

--- project/apps/account/src/app/blog-user/blog-user.repository.ts
+++ project/apps/account/src/app/blog-user/blog-user.repository.ts
@@ -1,4 +1,6 @@
+import { Injectable } from '@nestjs/common';
 import { BaseMemoryRepository } from '@project/shared/core';
 import { BlogUserEntity } from './blog-user.entity';
 
+@Injectable()
 export class BlogUserRepository extends BaseMemoryRepository<BlogUserEntity> {}
